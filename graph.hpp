1fdb0035a8c5571b663c9db971cd11c2e0700aaf
jhonata <jhonatadam@gmail.com>
2020-07-25 19:10:52 -0300

    Refactoring of Graph templates.

--- graph.hpp
+++ graph.hpp
@@ -1,32 +1,27 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 
-#include <vector>
 #include <list>
 #include <unordered_map>
 
 using namespace std;
 
-struct Vertex 
-{
-    long id;
-    Vertex(const long &id) : id(id) {}
-};
+struct None {};
 
-struct Edge
-{
-    long u_id;
-    long v_id;
-    Edge(const long &u_id, const long &v_id) :
-        u_id(u_id), v_id(v_id) {}
-};
-
-template <class Te = Edge, class Tv = Vertex>
+template <class T_edata = void*,
+          class T_vdata = void*,
+          class T_vertex = long>
 class Graph
 {
-    typedef unordered_map<long,Edge*> Adjacency;
-    typedef unordered_map<long,Adjacency> AdjacencyMap;
-    typedef unordered_map<long,Vertex> VertexMap;
+    typedef T_vertex Vertex;
+    struct Edge {
+        Vertex u;
+        Vertex v;
+        T_edata data;
+    };
+    typedef unordered_map<Vertex,Edge*> Adjacency;
+    typedef unordered_map<Vertex,Adjacency> AdjacencyMap;
+    typedef unordered_map<Vertex,T_vdata> VertexMap;
     typedef list<Edge> EdgeList;
     
     ////////////////////////////////////////////////////////////////
@@ -70,19 +65,20 @@ public:
     ///        the graph.
     /// \param u: vertex.
     ///
-    void add_vertex(const Vertex &u);
+    void add_vertex(const Vertex &u, const T_vdata &data = T_vdata());
 
     ////////////////////////////////////////////////////////////////
     /// \brief add_edge adds edge e.
     /// \param e: edge.
     ///
-    void add_edge(const Edge &e);
+    void add_edge(const Vertex &u, const Vertex &v,
+                  const T_edata data = T_edata());
 
     ////////////////////////////////////////////////////////////////
     /// \brief remove_vertex removes vertex u.
     /// \param u_id: ID of vertex u.
     ///
-    void remove_vertex(const long &u_id); // *
+    void remove_vertex(const Vertex &u); // *
 
     ////////////////////////////////////////////////////////////////
     /// \brief remove_edge removes the edge indentified by
@@ -98,7 +94,7 @@ public:
     /// \param u_id: vertex ID.
     /// \return a constant Vertex reference.
     ///
-    const Vertex& vertex(const long &u_id) const;
+    const T_vdata &vertex_data(const Vertex &u) const;
 
     ////////////////////////////////////////////////////////////////
     /// \brief vertex gives a constant access to the edge
@@ -106,14 +102,14 @@ public:
     /// \param u_id, v_id: IDs of the endpoints of the edge.
     /// \return a constant Edge reference.
     ///
-    const Edge& edge(const long &u_id, const long &v_id) const;
+    const T_edata& edge_data(const Vertex &u, const Vertex &v) const;
 
     ////////////////////////////////////////////////////////////////
     /// \brief has_vertex check if vertex u is in the graph
     /// \param u_id: ID of the vertex u.
     /// \return true if u in the graph, false otherwise.
     ///
-    bool has_vertex(const long &u_id) const;
+    bool has_vertex(const Vertex &u) const;
 
     ////////////////////////////////////////////////////////////////
     /// \brief has_edge
@@ -121,33 +117,33 @@ public:
     /// \param v_id
     /// \return
     ///
-    bool has_edge(const long &u_id, const long &v_id) const;
+    bool has_edge(const Vertex &u, const Vertex &v) const;
 
     ////////////////////////////////////////////////////////////////
     /// \brief vertices
     /// \return
     ///
-    vector<Vertex>& vertices() const; // *
+    void vertices() const; // *
 
     ////////////////////////////////////////////////////////////////
     /// \brief edges
     /// \return
     ///
-    vector<pair<Vertex, Vertex>>& edges() const; // *
+    void edges() const; // *
 
     ////////////////////////////////////////////////////////////////
     /// \brief subgraph
     /// \param subgraph_vertices
     /// \return
     ///
-    Graph * subgraph(vector<long> &subgraph_vertices) const; // *
+//    Graph * subgraph(vector<Vertex> &subgraph_vertices) const; // *
 
     ////////////////////////////////////////////////////////////////
     /// \brief subgraph
     /// \param subgraph_edges
     /// \return
     ///
-    Graph * subgraph(vector<pair<long, long>> &subgraph_edges) const; // *
+//    Graph * subgraph(vector<pair<long, long>> &subgraph_edges) const; // *
 
     // PROPERTIES
     ////////////////////////////////////////////////////////////////
@@ -155,7 +151,7 @@ public:
     /// \param u
     /// \return
     ///
-    vector<Vertex>& adjacency(Vertex u) const; // *
+    void adjacency(Vertex u) const; // *
 
     ////////////////////////////////////////////////////////////////
     /// \brief degree
@@ -184,41 +180,44 @@ public:
 
 };
 
-
-template<class Te, class Tv>
-Graph<Te, Tv>::Graph(bool directed, bool no_loops)
+template<class T_edata, class T_vdata, class T_vertex>
+Graph<T_edata, T_vdata, T_vertex>::Graph(
+        bool directed, bool no_loops)
 {
     this->directed = directed;
     this->no_loops = no_loops;
 }
 
-template<class Te, class Tv>
-void Graph<Te, Tv>::add_vertex(const Vertex &u)
+template<class T_edata, class T_vdata, class T_vertex>
+void Graph<T_edata, T_vdata, T_vertex>::add_vertex(
+        const Vertex &u, const T_vdata &data)
 {
-    if (!this->has_vertex(u.id)) {
-        this->vertex_map.insert({u.id, u});
+    if (!this->has_vertex(u)) {
+        this->vertex_map[u] = data;
     }
 }
 
-template<class Te, class Tv>
-void Graph<Te, Tv>::add_edge(const Edge &e)
+template<class T_edata, class T_vdata, class T_vertex>
+void Graph<T_edata, T_vdata, T_vertex>::add_edge(
+        const Vertex &u, const Vertex &v, T_edata data)
 {
-    if (this->no_loops && e.u_id == e.v_id)
+    if (this->no_loops && u == v)
         return;
 
-    if (!this->has_edge(e.u_id, e.v_id)) {
-        this->add_vertex(Vertex(e.u_id));
-        this->add_vertex(Vertex(e.v_id));
-        this->edge_list.push_back(e);
+    if (!this->has_edge(u, v)) {
+        this->add_vertex(u);
+        this->add_vertex(v);
+        this->edge_list.push_back({u, v, data});
 
-        this->adj_map[e.u_id][e.v_id] = &edge_list.back();
+        this->adj_map[u][v] = &edge_list.back();
         if (!this->directed)
-            this->adj_map[e.v_id][e.u_id] = &edge_list.back();
+            this->adj_map[v][u] = &edge_list.back();
     }
 }
 
-template<class Te, class Tv>
-void Graph<Te, Tv>::remove_vertex(const long &u_id)
+template<class T_edata, class T_vdata, class T_vertex>
+void Graph<T_edata, T_vdata, T_vertex>::remove_vertex(
+        const Vertex &u)
 {
     // // getting u iterator if it exists
     // auto u_it = this->adj_list.find(u);
@@ -235,43 +234,48 @@ void Graph<Te, Tv>::remove_vertex(const long &u_id)
     // }
 }
 
-template<class Te, class Tv>
-const Vertex & Graph<Te, Tv>::vertex(const long &u_id) const {
-    return this->vertex_map.at(u_id);
+template<class T_edata, class T_vdata, class T_vertex>
+const T_vdata & Graph<T_edata, T_vdata, T_vertex>::vertex_data(
+        const Vertex &u) const
+{
+    return this->vertex_map.at(u);
 }
 
-template<class Te, class Tv>
-const Edge & Graph<Te, Tv>::edge(const long &u_id, const long &v_id) const
+template<class T_edata, class T_vdata, class T_vertex>
+const T_edata & Graph<T_edata, T_vdata, T_vertex>::edge_data(
+        const Vertex &u, const Vertex &v) const
 {
-    return *this->adj_map.at(u_id).at(v_id);
+    return this->adj_map.at(u).at(v)->data;
 }
 
-template<class Te, class Tv>
-bool Graph<Te, Tv>::has_vertex(const long &u_id) const
+template<class T_edata, class T_vdata, class T_vertex>
+bool Graph<T_edata, T_vdata, T_vertex>::has_vertex(
+        const Vertex &u) const
 {
-    return this->vertex_map.find(u_id) != this->vertex_map.end();
+    return this->vertex_map.find(u) != this->vertex_map.end();
 }
 
-template<class Te, class Tv>
-bool Graph<Te, Tv>::has_edge(const long &u_id, const long &v_id) const
+template<class T_edata, class T_vdata, class T_vertex>
+bool Graph<T_edata, T_vdata, T_vertex>::has_edge(
+        const Vertex &u, const Vertex &v) const
 {
-    AdjacencyMap::const_iterator u_it = this->adj_map.find(u_id);
+    auto u_it = this->adj_map.find(u);
     if (u_it != this->adj_map.end()) {
         const Adjacency &u_adj = u_it->second;
-        if (u_adj.find(v_id) != u_adj.end())
+        if (u_adj.find(v) != u_adj.end())
             return true;
     }
     return false;
 }
 
-template<class Te, class Tv>
-size_t Graph<Te, Tv>::order() const
+template<class T_edata, class T_vdata, class T_vertex>
+size_t Graph<T_edata, T_vdata, T_vertex>::order() const
 {
     return vertex_map.size();
 }
 
-template<class Te, class Tv>
-size_t Graph<Te, Tv>::size() const
+template<class T_edata, class T_vdata, class T_vertex>
+size_t Graph<T_edata, T_vdata, T_vertex>::size() const
 {
     return edge_list.size();
 }
